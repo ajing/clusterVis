e339ba75171c550298aada88a6a19954ddceafe2
ajing <ajingnk@gmail.com>
2013-10-17 20:50:34 -0400

    test new neighbor join algorithm

--- BuildTree.py
+++ BuildTree.py
@@ -6,6 +6,7 @@ from ete2 import Tree, faces
 import datetime
 import math
 from hcluster import linkage, to_tree
+from TreeConstruction import *
 
 class ImgFace(faces.Face):
     def __init__(self, img_file, width=None, height=None):
@@ -31,6 +32,12 @@ class BuildTree():
         self.size    = len(leaderlist)
         self.drawTree()
 
+    def LeaderName( self, leaderlist ):
+        leadername = []
+        for each in leaderlist:
+            leadername.append(self.molDict[each]["ligandid"])
+        return leadername
+
     def LeaderMatrix( self, dmatrix, leaderlist ):
         return dmatrix[leaderlist, :][:, leaderlist]
 
@@ -39,48 +46,12 @@ class BuildTree():
         ligandFace    = ete2.faces.ImgFace( self.imgPath + imageFileName, 100, 100)
         return ligandFace
 
-    def prepareTree( self ):
-        # Y is a distance matrix for all elements
-        #hcluster part
-        Y = self.LeaderMatrix( self.distanceMatrix, self.leaderList )
-        N = Y.shape[0]
-        Z = linkage(Y, "single")
-        T = to_tree(Z)
-        #ete2 section
-        root = ete2.Tree()
-        root.dist = 0
-        root.name = "root"
-        item2node = {T: root}
-        to_visit = [T]
-        while to_visit:
-            node = to_visit.pop()
-            cl_dist = node.dist /2.0
-            for ch_node in [node.left, node.right]:
-                if ch_node:
-                    ch = ete2.Tree()
-                    ch.dist = cl_dist
-                    if ch_node.id < N:
-                        origID  = self.leaderList[ch_node.id]
-                        ch.name = self.molDict[ origID ]["ligandid"]
-                        style = ete2.NodeStyle()
-                        style["vt_line_width"] = self.figuresize/5
-                        style["hz_line_width"] = self.figuresize/5
-                        # give one more attribute for size
-                        #ch.size = ch_node.id
-                        try:
-                            style["size"] = math.log( self.molDict[ origID ]["size"] )
-                            ch.img_style = style
-                        except:
-                            print self.molDict[ origID ]
-                            raise LookupError("cannot find:" + str(origID))
-                        ligandFace = self.imageFace( ch_node.id )
-                        #ch.add_face( ligandFace, column = 1 )
-                    item2node[node].add_child(ch)
-                    item2node[ch_node] = ch
-                    to_visit.append(ch_node)
-        # This is your ETE tree structure
-        tree = root
-        return tree
+    def prepareNJ(self):
+        leader_name = self.LeaderName(self.leaderList)
+        d_shrink_matrix = self.LeaderMatrix( self.distanceMatrix, self.leaderList )
+        if not d_shrink_matrix.size[0] == len(leader_name):
+            raise ValueError("size doesn't match between leaderlist and distance matrix")
+        DMatrix     = DistanceMatrix(leader_name, d_shrink_matrix)
 
     def my_layout( self, node ):
         if not node.is_leaf():
