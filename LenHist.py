7c11ddd4156442612e3241f0e93bce8a2a793742
ajing <ajingnk@gmail.com>
2014-04-10 21:58:40 -0400

    more histogram for comparing statistics

--- LenHist.py
+++ LenHist.py
@@ -5,7 +5,7 @@ from TreeRebuilder import IsEdge, NameAndAttribute, GetAttributeValue
 import matplotlib.pyplot as plt
 import numpy
 
-def LenHist(dotfile):
+def EdgeHist(dotfile):
     # I assume dot file has len attribute for edge
     lenlist = []
     for line in open(dotfile):
@@ -16,11 +16,21 @@ def LenHist(dotfile):
     plt.hist(lenlist, 10)
     plt.show()
 
-if __name__ == "__main__":
+def EdgeHistMain():
     infile = "Data/all_0.9.gv_beforeMod"
     infile = "Data/all_0.3.gv"
     infile = "Data/09_simple.gv"
     infile = "Data/03_simple.gv"
     infile = "Data/09_simple_10_2.gv"
-    LenHist(infile)
+    EdgeHist(infile)
 
+def LenHist(numpyfile):
+    dmatrix = numpy.load(numpyfile)
+    print dmatriz.size
+
+def LenHistMain():
+    nfile = "Data/all_0.9.npz"
+    LenHist(nfile)
+
+if __name__ == "__main__":
+    LenHistMain()
